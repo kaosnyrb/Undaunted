49e717d7f16792aa148a4bf0e3952358c72ece21
Bryn Stringer <kaos.nyrb@gmail.com>
2020-06-02 15:41:57 +0100

    Base Vert and Triangle Type working and exporting.
    The basic elements of the Navmesh are now controllable in C++ via a SSE Edit script

--- Undaunted/NavMeshTool.cpp
+++ Undaunted/NavMeshTool.cpp
@@ -1,48 +1,78 @@
 #include "NavmeshTool.h"
 #include "SKSELink.h"
-
-std::string floattostring(float value)
+namespace Undaunted
 {
-	char hex[9];
-	sprintf(hex, "%08X", *(unsigned long int*) & value);
-	//	_MESSAGE("float value: %s", hex);
-	std::string output = "";
-	output += hex[6];
-	output += hex[7];
-	output += ' ';
-	output += hex[4];
-	output += hex[5];
-	output += ' ';
-	output += hex[2];
-	output += hex[3];
-	output += ' ';
-	output += hex[0];
-	output += hex[1];
-	return output;
-}
+	std::string floattostring(float value)
+	{
+		char hex[9];
+		sprintf(hex, "%08X", *(unsigned long int*) & value);
+		//	_MESSAGE("float value: %s", hex);
+		std::string output = "";
+		output += hex[6];
+		output += hex[7];
+		output += ' ';
+		output += hex[4];
+		output += hex[5];
+		output += ' ';
+		output += hex[2];
+		output += hex[3];
+		output += ' ';
+		output += hex[0];
+		output += hex[1];
+		return output;
+	}
 
-void Undaunted::ExportNavmesh()
-{
-	std::string output = floattostring(-32.0411339);
-	_MESSAGE("output value: %s", output.c_str());
-	/*
-	gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Navmesh.pas");
-	_MESSAGE("unit userscript;uses SkyrimUtils;uses mteFunctions;");
-	_MESSAGE("function NewArrayElement(rec: IInterface; path: String): IInterface; var a: IInterface; begin a := ElementByPath(rec, path); if Assigned(a) then begin Result := ElementAssign(a, HighInteger, nil, false); end else begin a := Add(rec, path, true);Result := ElementByIndex(a, 0);end;end;");
-	_MESSAGE("function Process(e: IInterface): integer; var cell: IInterface; navm: IInterface; nvnm: IInterface; verts: IInterface; begin Result := 0;  if not (Signature(e) = 'CELL') then begin Exit; end; AddMessage('Processing: ' + FullPath(e));");
-	_MESSAGE("navm := Add(e,'NAVM',true);nvnm := Add(navm,'NVNM',true);seev(nvnm, 'Version', 12);seev(nvnm, 'Parent Cell', HexFormID(e));seev(nvnm, 'NavMeshGrid', '00');");
+	std::string uint32tostring(UInt32 value)
+	{
+		char hex[9];
+		sprintf(hex, "%04X", *(unsigned long int*) & value);
+		std::string output = "";
+		output += hex[2];
+		output += hex[3];
+		output += ' ';
+		output += hex[0];
+		output += hex[1];
+		return output;
+	}
 
-	
-	//Float - Little Endian (DCBA) - x y z
-	_MESSAGE("verts := NewArrayElement(nvnm, 'Vertices');seev(nvnm, 'Vertices\\[0]', '1F 2A 00 C2 6C 32 26 C2 00 FF 7F BF');");
-	_MESSAGE("verts := NewArrayElement(nvnm, 'Vertices');seev(nvnm, 'Vertices\\[1]', '37 04 13 42 CC B6 78 C1 00 00 80 BF');");
-	_MESSAGE("verts := NewArrayElement(nvnm, 'Vertices');seev(nvnm, 'Vertices\\[2]', '88 E3 E2 41 EC D1 99 C2 00 FF 7F BF');");
+	void AddVertex(Vert vertex)
+	{
+		_MESSAGE("verts := NewArrayElement(nvnm, 'Vertices');seev(nvnm, 'Vertices\\[%i]', '%s %s %s');", vertex.index, floattostring(vertex.x), floattostring(vertex.y), floattostring(vertex.z));
+	}
 
-	// vert index UINT32 - vert index  UINT32 - vert index  UINT32 - edge neighbour triangle index UINT32 - cover flag - 
-	_MESSAGE("NewArrayElement(nvnm, 'Triangles');seev(nvnm, 'Triangles\\[0]', '00 00 02 00 01 00 FF FF FF FF FF FF 00 00 00 00');");
+	void AddTriangle(Triangle tri)
+	{
+		//Not bothering with cover right now, which is the last 4 values.
+		_MESSAGE("NewArrayElement(nvnm, 'Triangles');seev(nvnm, 'Triangles\\[%i]', '%s %s %s %s %s %s 00 00 00 00');",
+			tri.index, 
+			uint32tostring(tri.vert1), 
+			uint32tostring(tri.vert2),
+			uint32tostring(tri.vert3),
+			uint32tostring(tri.edge1),
+			uint32tostring(tri.edge2),
+			uint32tostring(tri.edge3));
+	}
 
+	// Remember to edit the navmesh slightly in the Creation Kit. Otherwise it seems to lead to an unending memory leak :D
+	// I believe this is due to us not setting up the NavMeshGrid in the code, howeer making any change in the CK and saving it generates it.
+	void ExportNavmesh()
+	{
+		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Navmesh.pas");
+		_MESSAGE("unit userscript;uses SkyrimUtils;uses mteFunctions;");
+		_MESSAGE("function NewArrayElement(rec: IInterface; path: String): IInterface; var a: IInterface; begin a := ElementByPath(rec, path); if Assigned(a) then begin Result := ElementAssign(a, HighInteger, nil, false); end else begin a := Add(rec, path, true);Result := ElementByIndex(a, 0);end;end;");
+		_MESSAGE("function Process(e: IInterface): integer; var cell: IInterface; navm: IInterface; nvnm: IInterface; verts: IInterface; begin Result := 0;  if not (Signature(e) = 'CELL') then begin Exit; end; AddMessage('Processing: ' + FullPath(e));");
+		_MESSAGE("navm := Add(e,'NAVM',true);nvnm := Add(navm,'NVNM',true);seev(nvnm, 'Version', 12);seev(nvnm, 'Parent Cell', HexFormID(e));seev(nvnm, 'NavMeshGrid', '00');");
 
-	_MESSAGE("end;end.");
-	gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Undaunted.log");
-	*/
+		//Float - Little Endian (DCBA) - x y z
+		AddVertex(Vert(0, -32.0411339, -41.5492, -0.999985));
+		AddVertex(Vert(1, -25.0411339, -75.5492, -0.999985));
+		AddVertex(Vert(2, 10.0411339, -41.5492, -0.999985));
+
+		// vert index UINT32 - vert index  UINT32 - vert index  UINT32 - edge neighbour triangle index UINT32 - cover flag - 
+		Triangle tri = Triangle(0,0,1,2,-1,-1,-1);
+		AddTriangle(tri);
+
+		_MESSAGE("end;end.");
+		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Undaunted.log");
+	}
 }
--- Undaunted/NavmeshTool.h
+++ Undaunted/NavmeshTool.h
@@ -6,13 +6,30 @@ namespace Undaunted
 	public:
 		UInt32 index;
 		float x, y, z;
+		Vert(UInt32 _index, float _x, float _y, float _z)
+		{
+			index = _index;
+			x = _x;
+			y = _y;
+			z = _z;
+		}
 	};
 
 	class Triangle {
 	public:
 		UInt32 index;
-		UInt32 vert1, vert2, vert3;
-		UInt32 edge1, edge2, edge3;
+		UInt32 vert1, vert2, vert3; //Index of a vertex
+		UInt32 edge1, edge2, edge3; //Index of a different triangle.
+		Triangle(UInt32 _index, UInt32 _vert1, UInt32 _vert2, UInt32 _vert3, UInt32 _edge1, UInt32 _edge2, UInt32 _edge3)
+		{
+			index = _index;
+			vert1 = _vert1;
+			vert2 = _vert2;
+			vert3 = _vert3;
+			edge1 = _edge1;
+			edge2 = _edge2;
+			edge3 = _edge3;
+		}
 	};
 
 	void ExportNavmesh();
